0327823f710ffcb1994952d95977380d929b868c
Tancredi Carluccio <tanc_@hotmail.it>
2022-03-16 12:50:29 +0100

    Prima bozza snack 3

--- snack-3/main.js
+++ snack-3/main.js
@@ -5,5 +5,7 @@ for (i=0; i<=10; i++){
     arrayDieci.push(numeri);
     let somma = somma + arrayDieci[i];
 
-    
+    if (i=10){
+        document.getElementById("esito").innerHTML = `<div>La somma di tutti i numeri inseriti è <span class="blue">"${somma}"</span> </div>`;    
+    }
 }
